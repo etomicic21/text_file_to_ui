f5e958f7dfaaa7ef0424e4213ce3cda9b85e3a08
emaat <etomicic21@student.foi.hr>
2023-05-04 18:53:39 +0200

    Implemented file reading #3

---
+++ TextToFile/UserFileReader.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextToFile
+{
+    public class UserFileReader
+    {
+        public string ReadFile(string directory)
+        {
+            return File.ReadAllText(directory);
+        }
+    }
+}
